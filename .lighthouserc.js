e6b16eaf53a5fef0c46f3773060569bb6856252d
pius ssekweyama <ssekweyamapius@gmail.com>
2022-11-22 17:32:11 +0300

    whitehouse

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,22 +2,9 @@ module.exports = {
     ci: {
       collect: {
         staticDistDir: './dist',
-        numberOfRuns: 1,
-        isSinglePageApplication: true,
-        settings: {
-          onlyCategories: ['accessibility', 'performance', 'seo']
-        },
-        assert: {
-          preset: 'lighthouse:no-pwa',
-          assertions: {
-            'categories:performance': ['error', { minScore: 0.1 }],
-            'categories:accessibility': ['error', { minScore: 0.1 }],
-            'categories:seo': ['error', { minScore: 0.1 }]
-          }
-        },
-        upload: {
-          target: 'temporary-public-storage'
-        },
-      }
-    }
+      },
+      upload: {
+        target: 'temporary-public-storage',
+      },
+    },
   };
